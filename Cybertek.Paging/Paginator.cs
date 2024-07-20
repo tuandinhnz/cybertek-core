d137462a2d2905093f96e2cb02cd7039861f893a
tuandinhnz <letuandinh@gmail.com>
2024-07-20 16:23:53 +1200

    Update Paginator.cs

--- Cybertek.Paging/Paginator.cs
+++ Cybertek.Paging/Paginator.cs
@@ -7,21 +7,21 @@ namespace Cybertek.Paging
 {
     public class Paginator : IPaginator
     {
-        private int _pageNumber;
-        private int _limit;
-        public const string PageNumberKey = "pageNumber";
+        private const string PageNumberKey = "pageNumber";
         private const string LimitKey = "limit";
         private const int DefaultPageNumber = 1;
         private const int DefaultLimit = 5;
         
         public int PageNumber { get; set; }
+
         public int Limit { get; set; }
-        public Uri ActionUri { get; set; }
+
+        public Uri? ActionUri { get; set; }
 
         private Paginator(int pageNumber, int limit)
         {
-            _pageNumber = pageNumber;
-            _limit = limit;
+            PageNumber = pageNumber;
+            Limit = limit;
         }
         public Paginator() : this(DefaultPageNumber, DefaultLimit)
         {
